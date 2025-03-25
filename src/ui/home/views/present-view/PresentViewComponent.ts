de248de2a10d71d4c239bf3b2fb486944a5be799
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-03-25 21:20:52 +0100

    Present view mock

--- src/ui/home/views/present-view/PresentViewComponent.ts
+++ src/ui/home/views/present-view/PresentViewComponent.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeLimitInputComponent } from '../../limit-input/HomeLimitInput';
 
 @Component({
 	selector: 'present-view',
+	imports: [
+		HomeLimitInputComponent
+	],
 	templateUrl: 'PresentViewComponent.html'
 })
 export class PresentViewComponent {
