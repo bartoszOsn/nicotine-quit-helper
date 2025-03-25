37bcae999ef14d64a34157fa7376977b80bcb874
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-03-25 21:09:58 +0100

    Future view

--- src/ui/home/views/future-view/FutureViewComponent.ts
+++ src/ui/home/views/future-view/FutureViewComponent.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeLimitInputComponent } from '../../limit-input/HomeLimitInput';
 
 @Component({
 	selector: 'future-view',
+	imports: [
+		HomeLimitInputComponent
+	],
 	templateUrl: 'FutureViewComponent.html'
 })
 export class FutureViewComponent {
