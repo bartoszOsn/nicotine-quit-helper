25fc5aab80cbf7c162416beec8b45d1c5f0e3f71
Bartosz Osiński <osinskibartosz24@gmail.com>
2025-03-27 01:17:21 +0100

    Notifications

--- src/ui/home/HomeRootComponent.ts
+++ src/ui/home/HomeRootComponent.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HomeDaySelectorComponent } from './day-selector/HomeDaySelectorComponent';
 import { Store } from '../../api/Store';
 import { AsyncPipe } from '@angular/common';
@@ -8,6 +8,7 @@ import { PresentViewComponent } from './views/present-view/PresentViewComponent'
 import { FutureViewComponent } from './views/future-view/FutureViewComponent';
 import { AppShellComponent } from '../common/app-shell/AppShellComponent';
 import { AppHeaderComponent } from '../common/app-header/AppHeaderComponent';
+import { NotificationTimerService } from './NotificationTimerService';
 
 @Component({
 	selector: 'home-root',
@@ -20,12 +21,18 @@ import { AppHeaderComponent } from '../common/app-header/AppHeaderComponent';
 		FutureViewComponent,
 		AppShellComponent,
 		AppHeaderComponent
-	]
+	],
+	providers: [NotificationTimerService]
 })
-export class HomeRootComponent {
+export class HomeRootComponent implements OnInit {
 	private readonly store = inject(Store);
+	private readonly notificationTimerService = inject(NotificationTimerService);
 
 	public readonly selectedDayTimeState$ = this.store.selectedDayTimeState$;
 
 	protected DayTimeState = DayTimeState;
+
+	ngOnInit(): void {
+		this.notificationTimerService.init();
+	}
 }
